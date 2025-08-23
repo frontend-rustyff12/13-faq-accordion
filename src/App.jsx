a2b6289dadee0664b414df5a3bc12fe938cf2c9f
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-23 07:11:56 +0200

    css transitions added. README.md update. a11y additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Accordian from "./components/Accordian";
 function App() {
   return (
-    <main className="min-h-screen bg-[url('/background-pattern-mobile.svg')] bg-no-repeat flex justify-center items-center bg-cust-Purple-100 py-32">
-      <div className="m-6 p-6 bg-cust-White rounded-xl">
+    <main className="min-h-screen bg-[url('/background-pattern-mobile.svg')] bg-no-repeat flex justify-center items-center bg-cust-Purple-100 py-32 md:bg-[url('/background-pattern-desktop.svg')]">
+      <div className="m-6 p-6 bg-cust-White rounded-xl md:w-xl md:p-8">
         <Accordian />
       </div>
     </main>
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -3,14 +3,18 @@ export default function Panel({ id, topic, answer, open, toggle }) {
     <article
       className="border-b-1 border-gray-300 last:border-b-0 pb-4"
       key={id}
+      role="region"
+      aria-labelledby={id}
     >
       <div className="mt-4">
         <button
-          className="flex justify-between items-center gap-4 w-full"
+          className="flex justify-between items-center gap-4 w-full cursor-pointer group"
           id={id}
           onClick={toggle}
+          aria-expanded={open}
+          aria-controls={`panel-content-${id}`}
         >
-          <span className="text-left text-base text-cust-Purple-950 font-bold font-work-sans">
+          <span className="text-left text-base text-cust-Purple-950 font-bold font-work-sans group-hover:text-cust-Purple-600">
             {topic}
           </span>
 
@@ -22,11 +26,11 @@ export default function Panel({ id, topic, answer, open, toggle }) {
         </button>
 
         <p
-          className={
-            !open
-              ? "hidden"
-              : "visible text-gray-500 font-work-sans my-4 text-sm"
-          }
+          id={`panel-content-${id}`}
+          className={`text-gray-500 font-work-sans my-4 text-sm overflow-hidden transition-all duration-700 ease-in-out ${
+            !open ? "max-h-0" : "max-h-screen"
+          }`}
+          aria-hidden={!open}
         >
           {answer}
         </p>
